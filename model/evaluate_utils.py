a8e7d4b81f02cf4123d28acac280461559211ff3
yingzibu <46125227+yingzibu@users.noreply.github.com>
2023-08-17 19:46:55 -0400

    Create evaluate_utils.py

---
+++ model/evaluate_utils.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def get_min(d:dict):
+    min_key = next(iter(d))
+
+    # Iterate over the keys in the dictionary
+    for key in d:
+        # If the value of the current key > the value of max_key, update max_key
+        if d[key] < d[min_key]:
+            min_key = key
+    return min_key, d[min_key]
+
+def plot_loss(train_dict, test_dict):
+    fig = plt.figure()
+    plt.plot(list(train_dict.keys()), list(train_dict.values()), label='train')
+    plt.plot(list(test_dict.keys()), list(test_dict.values()), label='test')
+    argmin, min = get_min(test_dict)
+    plt.plot(argmin, min, '*', label=f'min epoch {argmin}') 
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('loss during training')
+    plt.legend()
+    plt.show()

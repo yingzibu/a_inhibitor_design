c62b2666fb45eecdc0346f573af6ebaa3da3bcdc
yingzibu <46125227+yingzibu@users.noreply.github.com>
2023-08-18 23:12:32 -0400

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,4 @@
 from .regressor_multi import Regressor, fp_reg_dataset
 from .evaluate_utils import get_min, plot_loss
 from .classifier_binary import Classifier_binary, fp_dataset
-from chemical_vae import MolecularVAE
+from .chemical_vae import MolecularVAE
